c2032400819355825f92e7bd8434277a486caefc
Ajdalaju <muhammedajdal@gmail.com>
2021-09-23 19:37:06 +0530

    Converted palindrome to a function

--- palindrome.py
+++ palindrome.py
@@ -2,9 +2,12 @@
 x = input('Enter the word :')
 x = x.lower() #converts the word into lower case
 
-if x[::-1] == x: #reverse of the word equals to the word
- print('It is palindrome')
-else:
- print('It is not palindrome')
+def palindome(x):
+ # Set doc string
 
- 
+ if x[::-1] == x: #reverse of the word equals to the word
+  print('It is palindrome')
+ else:
+  print('It is not palindrome') 
+
+palindome(x)
